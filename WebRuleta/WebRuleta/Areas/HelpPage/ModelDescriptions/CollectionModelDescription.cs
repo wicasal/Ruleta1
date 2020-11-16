964a9121ede1c80109988cfb4206c049ca1da524
wilfredo.casallas <wilfredo.casallas@hotmail.com>
2020-11-16 15:19:55 -0500

    Proyecto

---
+++ WebRuleta/WebRuleta/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebRuleta.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ WebRuleta/WebRuleta/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebRuleta.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
